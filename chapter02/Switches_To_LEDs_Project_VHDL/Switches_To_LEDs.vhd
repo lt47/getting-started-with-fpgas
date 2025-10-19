e82f4287cff5ab65b9e0e1b6d780da7783e82a86
lt47 <layetee@gmail.com>
2025-10-19 14:50:57 -0500

    Making sure updated bhd code was added for chapter 2.

---
+++ chapter02/Switches_To_LEDs_Project_VHDL/Switches_To_LEDs.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity Switches_To_LEDR is
+  port (
+    SW: in std_logic_vector(9 downto 0); 
+	 LEDR: out std_logic_vector(9 downto 0)
+	 );
+end entity Switches_To_LEDR;
+
+architecture RTL of Switches_To_LEDR is
+begin
+
+  LEDR(0) <= SW(0);
+  LEDR(1) <= SW(1);
+  LEDR(2) <= SW(2);
+  LEDR(3) <= SW(3);
+  LEDR(4) <= SW(4);
+  LEDR(5) <= SW(5);
+  LEDR(6) <= SW(6);
+  LEDR(7) <= SW(7);
+  LEDR(8) <= SW(8);
+  LEDR(9) <= SW(9);
+  
+end architecture RTL;
+ 
